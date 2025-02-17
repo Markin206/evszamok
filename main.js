e784f40cdb8fed6a8cbe0162bb1c22058af9e115
Markin206 <marci061123@gmail.com>
2025-02-17 16:30:16 +0100

    valtozok bevezetese

--- main.js
+++ main.js
@@ -1,3 +1,35 @@
+//----------------------------------------------------------------------------------------- valuek
+const headerIdoszakValue = "Időszak"//változó létrehozása mely tartalmazza a headeridoszak string értékét
+const headerEvszamValue = "Évszám"//változó létrehozása mely tartalmazza a headerevszam string értékét
+const headerEsemenyValue = "Esemény"//változó létrehozása mely tartalmazza a headeresemeny string értékét
+const headerTananyagValue = "Tananyag"//változó létrehozása mely tartalmazza a headertananyag string értékét
+
+const tableBodyRow1Idoszak = "XVI. század"//változó létrehozása mely tartalmazza a idoszak string értékét
+const tableBodyRow3Idoszak = "XVII."//változó létrehozása mely tartalmazza a idoszak string értékét
+const tableBodyRow4Idoszak = "XVIII."//változó létrehozása mely tartalmazza a idoszak string értékét
+const tableBodyRow6Idoszak = "XIX."//változó létrehozása mely tartalmazza a idoszak string értékét
+
+const tableBodyRow1Evszam = "1514"//változó létrehozása mely tartalmazza a evszam string értékét
+const tableBodyRow2Evszam = "1519-1522"//változó létrehozása mely tartalmazza a evszam string értékét
+const tableBodyRow3Evszam = "1664"//változó létrehozása mely tartalmazza a evszam string értékét
+const tableBodyRow4Evszam = "1701-1714"//változó létrehozása mely tartalmazza a evszam string értékét
+const tableBodyRow5Evszam = "1703-1711"//változó létrehozása mely tartalmazza a evszam string értékét
+const tableBodyRow6Evszam = "1812"//változó létrehozása mely tartalmazza a evszam string értékét
+const tableBodyRow7Evszam = "1809"//változó létrehozása mely tartalmazza a evszam string értékét
+
+const tableBodyRow1Esemeny = "Dózsa-féle parasztháború"//változó létrehozása mely tartalmazza a esemeny string értékét
+const tableBodyRow2Esemeny = "Magellán körülhajózza a földet"//változó létrehozása mely tartalmazza a esemeny string értékét
+const tableBodyRow3Esemeny = "vasvári béke"//változó létrehozása mely tartalmazza a esemeny string értékét
+const tableBodyRow4Esemeny = "spanyol örökösödési háború"//változó létrehozása mely tartalmazza a esemeny string értékét
+const tableBodyRow5Esemeny = "Rákóczi szabadságharc"//változó létrehozása mely tartalmazza a esemeny string értékét
+const tableBodyRow6Esemeny = "Napóleon oroszországi hadjárata"//változó létrehozása mely tartalmazza a esemeny string értékét
+const tableBodyRow7Esemeny = "győri csata"//változó létrehozása mely tartalmazza a esemeny string értékét
+
+const tableBodyRow1Tananyag = "magyar"//változó létrehozása mely tartalmazza a tananyag string értékét
+const tableBodyRow2Tananyag = "egyetemes"//változó létrehozása mely tartalmazza a tananyag string értékét
+
+
+//----------------------------------------------------------------------------------------- tabla lineáris
 
 const tabla = document.createElement('table'); // Létrehozzuk a táblázatot
 document.body.appendChild(tabla); // Hozzáadjuk a táblázatot a body-hoz
@@ -9,20 +41,20 @@ tabla.appendChild(tablaheader); // Hozzáadjuk a headert a táblázathoz
 const tablaheaderRow = document.createElement('tr'); // Létrehozzuk a tablaheaderRow sort
 tablaheader.appendChild(tablaheaderRow); // Hozzáadjuk a tablaheaderRow sort a theadhez
 
-const periodo = document.createElement('th'); // Létrehozzuk az "Időszak" oszlopot
-periodo.innerHTML = "Időszak"; // Beállítjuk a "Időszak" szöveget
-tablaheaderRow.appendChild(periodo); // Hozzáadjuk a tablaheaderRow sorhoz
+const idoszak = document.createElement('th'); // Létrehozzuk az "Időszak" oszlopot
+idoszak.innerHTML = headerIdoszakValue; // Beállítjuk a "Időszak" szöveget a megadott változó értékére
+tablaheaderRow.appendChild(idoszak); // Hozzáadjuk a tablaheaderRow sorhoz
 
 const evszam = document.createElement('th'); // Létrehozzuk az "Évszám" oszlopot
-evszam.innerHTML = "Évszám"; // Beállítjuk az "Évszám" szöveget
+evszam.innerHTML = headerEvszamValue; // Beállítjuk az "Évszám" szöveget a megadott változó értékére
 tablaheaderRow.appendChild(evszam); // Hozzáadjuk a tablaheaderRow sorhoz
 
 const esemeny = document.createElement('th'); // Létrehozzuk az "Esemény" oszlopot
-esemeny.innerHTML = "Esemény"; // Beállítjuk az "Esemény" szöveget
+esemeny.innerHTML = headerEsemenyValue; // Beállítjuk az "Esemény" szöveget a megadott változó értékére
 tablaheaderRow.appendChild(esemeny); // Hozzáadjuk a tablaheaderRow sorhoz
 
 const tananyag = document.createElement('th'); // Létrehozzuk a "Tananyag" oszlopot
-tananyag.innerHTML = "Tananyag"; // Beállítjuk a "Tananyag" szöveget
+tananyag.innerHTML = headerTananyagValue; // Beállítjuk a "Tananyag" szöveget a megadott változó értékére
 tablaheaderRow.appendChild(tananyag); // Hozzáadjuk a tablaheaderRow sorhoz
 
 //----------------------------------------------------------------------------------------- tbody
@@ -35,19 +67,19 @@ tablaBody.appendChild(row1); // Hozzáadjuk a tablebodyhoz
 
 const idoszak1 = document.createElement('td'); // Létrehozzuk az "Időszak" cellát
 idoszak1.rowSpan = 2; // Beállítjuk a rowspan értéket 2-re
-idoszak1.innerHTML = "XVI. század"; // Beállítjuk a tartalmat
+idoszak1.innerHTML = tableBodyRow1Idoszak; // Beállítjuk a tartalmat a megadott változó értékére
 row1.appendChild(idoszak1); // Hozzáadjuk a sorhoz
 
 const ev1 = document.createElement('td'); // Létrehozzuk az "Évszám" cellát
-ev1.innerHTML = "1514"; // Beállítjuk a tartalmat
+ev1.innerHTML = tableBodyRow1Evszam; // Beállítjuk a tartalmat a megadott változó értékére
 row1.appendChild(ev1); // Hozzáadjuk a sorhoz
 
 const esemeny1 = document.createElement('td'); // Létrehozzuk az "Esemény" cellát
-esemeny1.innerHTML = "Dózsa-féle parasztháború"; // Beállítjuk a tartalmat
+esemeny1.innerHTML = tableBodyRow1Esemeny; // Beállítjuk a tartalmat a megadott változó értékére
 row1.appendChild(esemeny1); // Hozzáadjuk a sorhoz
 
 const tananyag1 = document.createElement('td'); // Létrehozzuk a "Tananyag" cellát
-tananyag1.innerHTML = "magyar"; // Beállítjuk a tartalmat
+tananyag1.innerHTML = tableBodyRow1Tananyag; // Beállítjuk a tartalmat a megadott változó értékére
 row1.appendChild(tananyag1); // Hozzáadjuk a sorhoz
 
 // 2. sor (XVI. század, Magellán körülhajózza a földet)
@@ -55,15 +87,15 @@ const row2 = document.createElement('tr'); // Létrehozzuk a 2. sort
 tablaBody.appendChild(row2); // Hozzáadjuk a tablebodyhoz
 
 const ev2 = document.createElement('td'); // Létrehozzuk az "Évszám" cellát
-ev2.innerHTML = "1519-1522"; // Beállítjuk a tartalmat
+ev2.innerHTML = tableBodyRow2Evszam; // Beállítjuk a tartalmat a megadott változó értékére
 row2.appendChild(ev2); // Hozzáadjuk a sorhoz
 
 const esemeny2 = document.createElement('td'); // Létrehozzuk az "Esemény" cellát
-esemeny2.innerHTML = "Magellán körülhajózza a földet"; // Beállítjuk a tartalmat
+esemeny2.innerHTML = tableBodyRow2Esemeny; // Beállítjuk a tartalmat a megadott változó értékére
 row2.appendChild(esemeny2); // Hozzáadjuk a sorhoz
 
 const tananyag2 = document.createElement('td'); // Létrehozzuk a "Tananyag" cellát
-tananyag2.innerHTML = "egyetemes"; // Beállítjuk a tartalmat
+tananyag2.innerHTML = tableBodyRow2Tananyag; // Beállítjuk a tartalmat a megadott változó értékére
 row2.appendChild(tananyag2); // Hozzáadjuk a sorhoz
 
 // 3. sor (XVII. század)
@@ -71,19 +103,19 @@ const row3 = document.createElement('tr'); // Létrehozzuk a 3. sort
 tablaBody.appendChild(row3); // Hozzáadjuk a tablebodyhoz
 
 const idoszak3 = document.createElement('td'); // Létrehozzuk az "Időszak" cellát
-idoszak3.innerHTML = "XVII. század"; // Beállítjuk a tartalmat
+idoszak3.innerHTML = tableBodyRow3Idoszak; // Beállítjuk a tartalmat a megadott változó értékére
 row3.appendChild(idoszak3); // Hozzáadjuk a sorhoz
 
 const ev3 = document.createElement('td'); // Létrehozzuk az "Évszám" cellát
-ev3.innerHTML = "1664"; // Beállítjuk a tartalmat
+ev3.innerHTML = tableBodyRow3Evszam; // Beállítjuk a tartalmat a megadott változó értékére
 row3.appendChild(ev3); // Hozzáadjuk a sorhoz
 
 const esemeny3 = document.createElement('td'); // Létrehozzuk az "Esemény" cellát
-esemeny3.innerHTML = "vasvári béke"; // Beállítjuk a tartalmat
+esemeny3.innerHTML = tableBodyRow3Esemeny; // Beállítjuk a tartalmat a megadott változó értékére
 row3.appendChild(esemeny3); // Hozzáadjuk a sorhoz
 
 const tananyag3 = document.createElement('td'); // Létrehozzuk a "Tananyag" cellát
-tananyag3.innerHTML = "magyar"; // Beállítjuk a tartalmat
+tananyag3.innerHTML = tableBodyRow1Tananyag; // Beállítjuk a tartalmat a megadott változó értékére
 row3.appendChild(tananyag3); // Hozzáadjuk a sorhoz
 
 // 4. sor (XVIII. század)
@@ -92,19 +124,19 @@ tablaBody.appendChild(row4); // Hozzáadjuk a tablebodyhoz
 
 const idoszak4 = document.createElement('td'); // Létrehozzuk az "Időszak" cellát
 idoszak4.rowSpan = 2;; // Beállítjuk a rowspan értéket 2-re
-idoszak4.innerHTML = "XVIII. század"; // Beállítjuk a tartalmat
+idoszak4.innerHTML = tableBodyRow4Idoszak; // Beállítjuk a tartalmat a megadott változó értékére
 row4.appendChild(idoszak4); // Hozzáadjuk a sorhoz
 
 const ev4 = document.createElement('td'); // Létrehozzuk az "Évszám" cellát
-ev4.innerHTML = "1701-1714"; // Beállítjuk a tartalmat
+ev4.innerHTML = tableBodyRow4Evszam; // Beállítjuk a tartalmat a megadott változó értékére
 row4.appendChild(ev4); // Hozzáadjuk a sorhoz
 
 const esemeny4 = document.createElement('td'); // Létrehozzuk az "Esemény" cellát
-esemeny4.innerHTML = "spanyol örökösödési háború"; // Beállítjuk a tartalmat
+esemeny4.innerHTML = tableBodyRow4Esemeny; // Beállítjuk a tartalmat a megadott változó értékére
 row4.appendChild(esemeny4); // Hozzáadjuk a sorhoz
 
 const tananyag4 = document.createElement('td'); // Létrehozzuk a "Tananyag" cellát
-tananyag4.innerHTML = "egyetemes"; // Beállítjuk a tartalmat
+tananyag4.innerHTML = tableBodyRow2Tananyag; // Beállítjuk a tartalmat a megadott változó értékére
 row4.appendChild(tananyag4); // Hozzáadjuk a sorhoz
 
 // 5. sor (XVIII. század, Rákóczi szabadságharc)
@@ -112,15 +144,15 @@ const row5 = document.createElement('tr'); // Létrehozzuk az 5. sort
 tablaBody.appendChild(row5); // Hozzáadjuk a tablebodyhoz
 
 const ev5 = document.createElement('td'); // Létrehozzuk az "Évszám" cellát
-ev5.innerHTML = "1703-1711"; // Beállítjuk a tartalmat
+ev5.innerHTML = tableBodyRow5Evszam; // Beállítjuk a tartalmat a megadott változó értékére
 row5.appendChild(ev5); // Hozzáadjuk a sorhoz
 
 const esemeny5 = document.createElement('td'); // Létrehozzuk az "Esemény" cellát
-esemeny5.innerHTML = "Rákóczi szabadságharc"; // Beállítjuk a tartalmat
+esemeny5.innerHTML = tableBodyRow5Esemeny; // Beállítjuk a tartalmat a megadott változó értékére
 row5.appendChild(esemeny5); // Hozzáadjuk a sorhoz
 
 const tananyag5 = document.createElement('td'); // Létrehozzuk a "Tananyag" cellát
-tananyag5.innerHTML = "magyar"; // Beállítjuk a tartalmat
+tananyag5.innerHTML = tableBodyRow1Tananyag; // Beállítjuk a tartalmat a megadott változó értékére
 row5.appendChild(tananyag5); // Hozzáadjuk a sorhoz
 
 // 6. sor (XIX. század)
@@ -129,19 +161,19 @@ tablaBody.appendChild(row6); // Hozzáadjuk a tablebodyhoz
 
 const idoszak6 = document.createElement('td'); // Létrehozzuk az "Időszak" cellát
 idoszak6.rowSpan = 2;; // Beállítjuk a rowspan értéket 2-re
-idoszak6.innerHTML = "XIX. század"; // Beállítjuk a tartalmat
+idoszak6.innerHTML = tableBodyRow6Idoszak; // Beállítjuk a tartalmat a megadott változó értékére
 row6.appendChild(idoszak6); // Hozzáadjuk a sorhoz
 
 const ev6 = document.createElement('td'); // Létrehozzuk az "Évszám" cellát
-ev6.innerHTML = "1812"; // Beállítjuk a tartalmat
+ev6.innerHTML = tableBodyRow6Evszam; // Beállítjuk a tartalmat a megadott változó értékére
 row6.appendChild(ev6); // Hozzáadjuk a sorhoz
 
 const esemeny6 = document.createElement('td'); // Létrehozzuk az "Esemény" cellát
-esemeny6.innerHTML = "Napóleon oroszországi hadjárata"; // Beállítjuk a tartalmat
+esemeny6.innerHTML = tableBodyRow6Esemeny; // Beállítjuk a tartalmat a megadott változó értékére
 row6.appendChild(esemeny6); // Hozzáadjuk a sorhoz
 
 const tananyag6 = document.createElement('td'); // Létrehozzuk a "Tananyag" cellát
-tananyag6.innerHTML = "egyetemes"; // Beállítjuk a tartalmat
+tananyag6.innerHTML = tableBodyRow2Tananyag; // Beállítjuk a tartalmat a megadott változó értékére
 row6.appendChild(tananyag6); // Hozzáadjuk a sorhoz
 
 // 7. sor (XIX. század, győri csata)
@@ -149,13 +181,13 @@ const row7 = document.createElement('tr'); // Létrehozzuk a 7. sort
 tablaBody.appendChild(row7); // Hozzáadjuk a tablebodyhoz
 
 const ev7 = document.createElement('td'); // Létrehozzuk az "Évszám" cellát
-ev7.innerHTML = "1809"; // Beállítjuk a tartalmat
+ev7.innerHTML = tableBodyRow7Evszam; // Beállítjuk a tartalmat a megadott változó értékére
 row7.appendChild(ev7); // Hozzáadjuk a sorhoz
 
 const esemeny7 = document.createElement('td'); // Létrehozzuk az "Esemény" cellát
-esemeny7.innerHTML = "győri csata"; // Beállítjuk a tartalmat
+esemeny7.innerHTML = tableBodyRow7Esemeny; // Beállítjuk a tartalmat a megadott változó értékére
 row7.appendChild(esemeny7); // Hozzáadjuk a sorhoz
 
 const tananyag7 = document.createElement('td'); // Létrehozzuk a "Tananyag" cellát
-tananyag7.innerHTML = "magyar"; // Beállítjuk a tartalmat
+tananyag7.innerHTML = tableBodyRow1Tananyag; // Beállítjuk a tartalmat a megadott változó értékére
 row7.appendChild(tananyag7); // Hozzáadjuk a sorhoz
